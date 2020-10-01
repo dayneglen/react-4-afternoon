c2cb6f374016375d52146a5c6ca0c2fad2e51bb6
dayneglen <dayneglen@gmail.com>
2020-10-01 14:40:31 -0600

    finished afternoon project

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import History from '../History/History';
+import Contact from '../Contact/Contact';
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <div className='subnav'>
+          <Link className='subnav_links' to='/about'>
+            <h3>About</h3>
+          </Link>
+          <Link className='subnav_links' to='/about/history'>
+            <h3>History</h3>
+          </Link>
+          <Link className='subnav_links' to='/about/contact'>
+            <h3>Contact</h3>
+          </Link>
         </div>
         <div className='box'>
+          <Switch>
+            <Route path='/about/history' component={History} />
+            <Route path='/about/contact' component={Contact} />
+            <Route 
+              exact path ='/about' 
+              render={() => (
+                <div>
+                  <h1>Here is some info on the university</h1>
+                </div>
+              )}
+              />
+          </Switch>
         </div>
       </div>
     )
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,36 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+    this.state = {
+      students: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+    .then(res => {
+      this.setState({students: res.data});
+    }).catch(err => console.log(err));
   }
 
   render() {
+    const classList = this.state.students.map((student, i) => (
+      <Link key={i} to={`/student/${student.id}`} >
+        <h3 >{`${student.first_name} ${student.last_name}`}</h3>
+      </Link>
+      
+    ));
+
     return (
       <div className="box">
         <h1></h1>
         <h2>ClassList:</h2>
-
+        {classList}
       </div>
     )
   }
